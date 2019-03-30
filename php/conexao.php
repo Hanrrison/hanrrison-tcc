e0e2cb66d1e650b40a061c8cc3036a1d10284646
Hanrrison <40416355+Hanrrison@users.noreply.github.com>
2019-03-30 18:43:14 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,3 +1,3 @@
 <?php
 mysqli_connect("alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	", "lwk8f7zk8565v63f", "lwk8f7zk8565v63f", "lwk8f7zk8565v63f") or die(mysqli_error());
-echo "Connected to MySQL<br />";
+echo "Connected to MySQL<br />";?>
