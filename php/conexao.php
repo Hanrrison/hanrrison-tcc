7c0831a1a9d2d0c21fc061606f999eb2e5374ad4
Hanrrison <40416355+Hanrrison@users.noreply.github.com>
2019-03-30 18:42:35 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,16 +1,3 @@
 <?php
-
-define('SERVER', 'alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('BANCO', 'a5cs5fjis77aj3b5');
-define('SENHA', 'z9zpne3a9ulmh9hg');
-define('USER', 'lwk8f7zk8565v63f');
-
-try{
-
-$con = new pdo('mysql:host=' . SERVER . ';dbname=' . BANCO, USER, SENHA);
-
-}catch(PDOException $e){
-echo "Erro gerado " . $e->getMessage(); 
-}
-
-?>
+mysqli_connect("alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	", "lwk8f7zk8565v63f", "lwk8f7zk8565v63f", "lwk8f7zk8565v63f") or die(mysqli_error());
+echo "Connected to MySQL<br />";
