d72d96da74ecc67469202033e5c8b3f783856e8f
Hanrrison <40416355+Hanrrison@users.noreply.github.com>
2019-03-31 14:20:22 -0300

    Update usuario.php

--- class/usuario.php
+++ class/usuario.php
@@ -8,11 +8,6 @@ class Usuario {
     public function conectar($db, $host, $user, $password){
 
         global $conn;
-                
-        $host = "alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $db = "a5cs5fjis77aj3b5";
-        $user = "lwk8f7zk8565v63f";
-        $password = "z9zpne3a9ulmh9hg";
         
         try {
             $conn = new PDO("mysql:host=$host; dbname=$db", $user, $password);
