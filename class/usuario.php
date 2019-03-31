d20c0971a316c0209c8cea561e773417425176c8
Hanrrison <40416355+Hanrrison@users.noreply.github.com>
2019-03-31 00:39:29 -0300

    Add files via upload

---
+++ class/usuario.php
@@ -0,0 +1,67 @@
+<?php
+
+class Usuario {
+    
+    private $conn;
+    public $msgErro = "";
+    
+    public function conectar($db, $host, $user, $password){
+
+        global $conn;
+        try {
+            $conn = new PDO("mysql:host=$host; dbname=$db", $user, $password);
+            return array("conexao" => $conn, "mensagem" => "Conectado no banco com sucesso");
+        } catch (PDOException $e) {
+            return array("conexao" => null, "mensagem" => "Ocorreu o erro ao se conectar:<br>" . $e->getMessage());
+        }
+
+    }
+
+    public function cadastro_usuario($usuario, $email, $senha){
+        global $conn;
+        //verificar se o nome ja tem cadastro
+        //se nao cadastrar
+        $sql = $conn->prepare("SELECT id_usuario FROM usuarios
+        WHERE usuario = :u");
+        $sql->bindValue(":u", $usuario);
+        $sql->execute();
+        if($sql->rowCount() > 0){
+            return false; //ja esta cadastrado no banco
+        } else{ //cadastrando no banco
+            $sql = $conn->prepare("INSERT INTO usuarios ($usuario, $email, $senha) 
+            VALUES (:u, :e, :s)");
+          $sql->bindValue(":u", $usuario);
+          $sql->bindValue(":e", $email);
+          $sql->bindValue(":s", md5($senha));
+          return true; //cadastrado com sucesso         
+        }
+
+    }
+
+    public function login($usuario, $senha){
+        global $conn;
+        //verificar se o usuario ja esta cadastrado, caso sim
+
+        $sql = $conn->prepare("SELECT id_usuario FROM usuarios
+        WHERE usuario = :u AND senha = :s");
+        $sql->bindValue(":u", $usuario);
+        $sql->bindValue(":s", md5($senha));
+        $sql->execute();
+        if($sql->rowCount() > 0){
+            //entrando no sistema
+            $dados = $sql->fetch();//transforma os dados do banco em array
+            session_start();
+            $_SESSION['id_usuario'] = $dados['id_usuario'];
+            return true; //usuario logado com sucesso
+
+        } else{
+            return false; //nao foi possivel logar
+        }
+
+
+    }
+    
+}
+
+
+?>
--- php/login.php
+++ php/login.php
@@ -1,4 +1,17 @@
 <?php
-  $db = new PDO("mysql:host=alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=a5cs5fjis77aj3b5", "lwk8f7zk8565v63f", "z9zpne3a9ulmh9hg");
-  unset($db);
+
+//variaveis usuario, senha
+
+include('conexao.php');
+
+if(empty($_POST['usuario']) || empty($_POST['senha'])){
+  header('Location: login.html');
+  exit();
+}
+
+
 ?>
+
+
+
+
